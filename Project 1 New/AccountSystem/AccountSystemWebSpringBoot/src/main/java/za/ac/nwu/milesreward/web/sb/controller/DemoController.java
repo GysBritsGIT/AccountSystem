bc5fa9d8dc80939f231844aa41a9e26247e61251
GysBritsGIT <91754356+GysBritsGIT@users.noreply.github.com>
2021-10-05 07:34:09 +0200

    Commit

--- Project 1 New/AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/milesreward/web/sb/controller/DemoController.java
+++ Project 1 New/AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/milesreward/web/sb/controller/DemoController.java
@@ -1,6 +1,5 @@
 package za.ac.nwu.milesreward.web.sb.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +9,6 @@ import za.ac.nwu.domain.dto.Demo;
 public class DemoController {
     //@GetMapping("/ping")
     //public GeneralResponse<String> ping(@RequestParam(value = "echo", defaultValue = "pong") String echo) {
-      //return new GeneralResponse<String>(successful = true, echo);
+    //return new GeneralResponse<String>(successful = true, echo);
     //}
 }
