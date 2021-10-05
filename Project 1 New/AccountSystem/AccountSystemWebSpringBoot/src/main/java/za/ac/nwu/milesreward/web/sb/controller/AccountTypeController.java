058ee7c5db38ee3ff3839793845f6caa6d903289
GysBritsGIT <91754356+GysBritsGIT@users.noreply.github.com>
2021-10-05 11:30:35 +0200

    Commit

--- Project 1 New/AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/milesreward/web/sb/controller/AccountTypeController.java
+++ Project 1 New/AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/milesreward/web/sb/controller/AccountTypeController.java
@@ -1,15 +1,29 @@
 package za.ac.nwu.milesreward.web.sb.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.as.domain.service.GeneralResponse;
 
 @RestController
+@RequestMapping("account-Type")
 public class AccountTypeController {
-    @GetMapping("/ping")
-    public GeneralResponse<String> ping(@RequestParam(value = "echo", defaultValue = "pong") String echo)
-    {
-    return new GeneralResponse<String>( true , echo);
+    @GetMapping("/all")
+    @ApiOperation(value = "Gets all the configured Account types.", notes = "Return a list of account types")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account types returned", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "Not Found", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal server error", response = GeneralResponse.class)})
+
+    public ResponseEntity<GeneralResponse<String>> getAll() {
+      GeneralResponse<String> response = new GeneralResponse<>(true, "No types found");
+    return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
